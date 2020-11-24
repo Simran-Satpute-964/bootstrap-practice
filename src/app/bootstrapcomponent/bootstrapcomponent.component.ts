025e763fb98570052abbdaaddd7d8336851b5d57
sx042f <simran.satpute@pdgc.com>
2020-11-24 12:14:13 +0530

    Components

--- src/app/bootstrapcomponent/bootstrapcomponent.component.ts
+++ src/app/bootstrapcomponent/bootstrapcomponent.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnChanges,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-bootstrapcomponent',
   templateUrl: './bootstrapcomponent.component.html',
-  styleUrls: ['./bootstrapcomponent.component.css']
+  styleUrls: ['./bootstrapcomponent.component.css'],
 })
-export class BootstrapcomponentComponent implements OnInit {
+export class BootstrapcomponentComponent implements OnInit, OnChanges {
   counter: number = 0;
-  constructor() { }
+  @ViewChild('input') input: ElementRef;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges() {}
 
   onCount(event: any) {
     if (event) {
@@ -21,5 +31,29 @@ export class BootstrapcomponentComponent implements OnInit {
       // console.table({count});
       // console.info('count', count);   // ctrl+shift+L
     }
+  }
+
+  check(event: any) {
+    if (this.input.nativeElement.value == "good" ||
+        this.input.nativeElement.value == "fine" ||
+        this.input.nativeElement.value == "tired") {
+      this.input.nativeElement.setCustomValidity('"' + this.input.nativeElement.value + '" is not a feeling.');
+    } else {
+      // input is fine -- reset the error message
+      this.input.nativeElement.setCustomValidity('');
+    }
+  }
+
+  @HostListener('submit', ['$event']) onPageLoad(event: Event) {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = Array.from(document.getElementsByClassName('needs-validation'));
+    let form = <HTMLFormElement>(forms[0]);
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
   }
 }
